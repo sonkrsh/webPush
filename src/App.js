0adb61cf1aae323910a5002f3ef8e8ffbb185941
Sourav <sourav.singh@emoha.com>
2023-06-25 09:55:21 +0530

    fix: success

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import { useEffect, useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import clevertap from "clevertap-web-sdk";
 
 function App() {
   // const AccountId = "84W-6W5-746Z";
-  const AccountId = "86K-85R-746Z";
+  const AccountId = "TEST-84W-6W5-746Z";
   const region = "in1";
 
   const cleverTapTrackEvents = (
@@ -13,8 +11,8 @@ function App() {
     eventProperties = {
       Secondary_Status: "Hungup",
       Status: "Contactable",
-      lead_owner_mobile_number: "+91 433223323",
-      lead_owner_name: "+91 433223323",
+      lead_owner_mobile_number: "+91 223323",
+      lead_owner_name: "+91 223323",
     }
   ) => {
     clevertap.event.push(eventName, eventProperties && eventProperties);
@@ -22,9 +20,9 @@ function App() {
 
   const handleOnUserLogin = () => {
     const userObj = {
-      Name: "TEST__TEST",
-      Identity: "12e7e8e8-0dd4-47c5-a784-654360b5d4df",
-      Phone: `+${"91" + "2233223323"}`,
+      Name: "TEST__TEST__",
+      Identity: "12e7e8e8-0dd4-47c5-a784-654360b5dhym",
+      Phone: `+${"91" + "223323"}`,
     };
     clevertap.onUserLogin.push({
       Site: userObj,
@@ -34,9 +32,9 @@ function App() {
 
   const handleOnUserProfileUpdate = () => {
     const userObj = {
-      Name: "TEST__TEST",
-      Identity: "12e7e8e8-0dd4-47c5-a784-654360b5d4df",
-      Phone: `+${"91" + "2233223323"}`,
+      Name: "TEST__TEST__update",
+      Identity: "12e7e8e8-0dd4-47c5-a784-654360b5dhym",
+      Phone: `+${"91" + "223323"}`,
       "MSG-email": true, // Disable email notifications
       "MSG-push": true, // Enable push notifications
       "MSG-sms": true, // Enable sms notifications
@@ -60,6 +58,24 @@ function App() {
       serviceWorkerPath: "/clevertap_sw.js",
     });
   };
+
+  function deleteAllCookies() {
+    const cookies = document.cookie.split(";");
+
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i];
+      const eqPos = cookie.indexOf("=");
+      const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+      document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+  }
+
+  const handleLogout = () => {
+    sessionStorage.clear();
+    localStorage.clear();
+    window.location.reload();
+    deleteAllCookies();
+  };
   return (
     <div
       className="App"
@@ -75,6 +91,7 @@ function App() {
         Call Profile Update
       </button>
       <button onClick={() => handleInit()}>INIT</button>
+      <button onClick={() => handleLogout()}>LogOut</button>
     </div>
   );
 }
